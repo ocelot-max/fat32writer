36c7e2737b2ceed6078045726bc60483f6234054
BuildTools <unconfigured@null.spigotmc.org>
2022-03-17 09:29:13 +0800

    qwq

--- src/fs/devices.c
+++ src/fs/devices.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 struct data_pool
 // 数据缓冲池
---
+++ src/test/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <devices.h>
+
+int main()
+{
+    devices_map_init();
+    return 0;
+}
