35ddf3c2bb26417d82a89fca14eafed1618b929a
ocelot-max <3310404226@qq.com>
2022-03-19 10:50:58 +0800

    awa

--- src/include/devices.h
+++ src/include/devices.h
@@ -2,6 +2,11 @@
 #define _DEVICES_H
 
 void devices_map_init();
+unsigned long loop_device(char *path);
+int deloop_device(unsigned long de_id);
+void list_devices();
+char * search_device_by_id(unsigned long s_id);
+unsigned long search_device_by_path(char *path);
 unsigned long write_sectors(unsigned long dev_id,
 		unsigned long start_sector,
 		unsigned long sectors,
